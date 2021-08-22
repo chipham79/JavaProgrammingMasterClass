e6934887c9740806bf280cfc2b1478596a2d8338
Chi Pham <69028376+chipham79@users.noreply.github.com>
2021-08-22 22:30:55 +0700

    Class1

---
+++ Section6oop/Car/TestCar.java
@@ -0,0 +1,10 @@
+package Section6oop.Car;
+
+public class TestCar {
+	public static void main(String[] args) {
+		Car porsche = new Car();
+		porsche.setModel("Carrera");
+		
+		System.out.println(porsche.getModel());
+	}
+}
