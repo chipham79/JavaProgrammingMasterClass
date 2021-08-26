c8e8eb288a95efee8e4313590c66b2e9d8b1d637
Chi Pham <69028376+chipham79@users.noreply.github.com>
2021-08-26 21:11:31 +0700

    inheritant

---
+++ Section6oop/inheritance/MainTest.java
@@ -0,0 +1,11 @@
+package Section6oop.inheritance;
+
+public class MainTest {
+	public static void main(String[] args) {
+		Animal animal = new Animal("Animal", 1, 1, 5, 5);
+		
+		Dog dog = new Dog("York", 8, 20, 2, 4, 1, 20, "long silky");
+		dog.eat();
+		dog.run();
+	}
+}
