5a72b6ffdc50f90bb044537df54a348126ec84e7
Chi Pham <69028376+chipham79@users.noreply.github.com>
2021-08-28 23:19:18 +0700

    compositionPractice

---
+++ oop2/composition/house/Wall.java
@@ -0,0 +1,16 @@
+package oop2.composition.house;
+
+public class Wall {
+	private String direction;
+
+	public Wall(String direction) {
+		this.direction = direction;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+	
+	
+	
+}
